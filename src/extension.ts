252d9c19bab684d37f7472c0a3001f990cde3346
Robin Bozan <robinbozan@gmail.com>
2021-09-12 13:34:33 +0200

    Adds VSCode settings

--- src/extension.ts
+++ src/extension.ts
@@ -12,49 +12,39 @@ import {
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  // let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+  const translationFiles = vscode.workspace.getConfiguration("lsp-translations").get<string[]>("translationFiles");
   let serverOptions: ServerOptions = {
-    // run: { module: serverModule, transport: TransportKind.ipc },
     run: {
       command: `--todo run --manifest-path ${context.asAbsolutePath(path.join('lsp-translations', 'Cargo.toml'))}`,
     },
     debug: {
-      // command: './lsp-translations',
       command: `cargo`,
       args: ['run'],
-      // args: ['watch', '-x', '"run"'],
-      // transport: { kind: TransportKind.socket, port: 9258 },
       transport: TransportKind.stdio,
       options: {
         cwd: context.asAbsolutePath('lsp-translations'),
-        // cwd: context.asAbsolutePath(path.join('lsp-translations', 'target', 'debug')),
         shell: true,
-        // detached: true
       }
     },
   };
 
-
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
    revealOutputChannelOn: RevealOutputChannelOn.Info,
-    outputChannelName: "jaja!",
-    progressOnInitialization: true,
     documentSelector: [{ scheme: "file", language: "javascript" }],
     synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      // fileEvents: vscode.workspace.createFileSystemWatcher("**/*.js"),
-      // fileEvents: vscode.workspace.createF
+      fileEvents: translationFiles?.map((globPattern) => vscode.workspace.createFileSystemWatcher(globPattern)),
     },
   };
-  vscode.window.showInformationMessage("test")
+  vscode.window.showInformationMessage("loaded!");
+  vscode.workspace.onDidChangeConfiguration((e) => {
+    vscode.window.showInformationMessage('Updated');
+  });
 
   // Create the language client and start the client.
   client = new LanguageClient(
     "lsp-translations",
-    "Language Server Example",
+    "lsp-translations",
     serverOptions,
     clientOptions
   );
